8b01f9b28ae5db5f63cb1dc503e45adba5ac814a
Bastian Reitemeier <mail@brtmr.de>
2019-02-23 21:06:01 +0100

    Notes module compiles. More documentation

--- src/Notes.elm
+++ src/Notes.elm
@@ -1,4 +1,9 @@
-module Notes exposing (noteNames)
+module Notes exposing (intervalName, midiToOctave, midiToPitch, noteName)
+
+import Dict exposing (..)
+import Result exposing (..)
+
+
 
 -- This module contains all the music theory necessary to calculate
 -- Minor, Major and Pentatonic scales
@@ -9,53 +14,85 @@ module Notes exposing (noteNames)
 -- Strings.
 
 
-noteNames : List ( String, String )
+noteNames : Dict Int ( String, String )
 noteNames =
-    [ ( "C", "C" )
-    , ( "C♯", "D♭" )
-    , ( "D", "D" )
-    , ( "E♭", "D♯" )
-    , ( "E", "E" )
-    , ( "F", "F" )
-    , ( "F♯", "G♭" )
-    , ( "G", "G" )
-    , ( "A♭", "G♯" )
-    , ( "A", "A" )
-    , ( "B♭", "A♯" )
-    , ( "B", "B" )
-    ]
+    Dict.fromList
+        [ ( 0, ( "C", "C" ) )
+        , ( 1, ( "C♯", "D♭" ) )
+        , ( 2, ( "D", "D" ) )
+        , ( 3, ( "E♭", "D♯" ) )
+        , ( 4, ( "E", "E" ) )
+        , ( 5, ( "F", "F" ) )
+        , ( 6, ( "F♯", "G♭" ) )
+        , ( 7, ( "G", "G" ) )
+        , ( 8, ( "A♭", "G♯" ) )
+        , ( 9, ( "A", "A" ) )
+        , ( 10, ( "B♭", "A♯" ) )
+        , ( 11, ( "B", "B" ) )
+        ]
+
+
+noteName : Int -> Result String ( String, String )
+noteName n =
+    case Dict.get n noteNames of
+        Just name ->
+            Result.Ok name
+
+        Nothing ->
+            Result.Err "note is not in the Interval 0..11"
 
 
 
 -- Maps the number of semitones in an interval to its name.
 -- The IntervalNames are the Names referring to the major/minor scale, where
 -- an interval is either minor/major or perfect.
+-- https://en.wikipedia.org/wiki/Interval_(music)#Main_intervals
+
+
+intervalNames : Dict Int String
+intervalNames =
+    Dict.fromList
+        [ ( 0, "Perfect unison" )
+        , ( 1, "Minor second" )
+        , ( 2, "Major second" )
+        , ( 3, "Minor third" )
+        , ( 4, "Major third" )
+        , ( 5, "Perfect fourth" )
+        , ( 7, "Perfect fifth" )
+        , ( 8, "Minor sixth" )
+        , ( 9, "Major sixth" )
+        , ( 10, "Minor seventh" )
+        , ( 11, "Major seventh" )
+        , ( 12, "Perfect octave" )
+        ]
 
 
 intervalName : Int -> Result String String
-intervalName 0 =
-    Ok "Perfect unison"
-intervalName 1 =
-    Ok "Minor second"
-intervalName 2 =
-    Ok "Major second"
-intervalName 3 =
-    Ok "Minor third"
-intervalName 4 =
-    Ok "Major third"
-intervalName 5 =
-    Ok "Perfect fourth"
-intervalName 7 =
-    Ok "Perfect fifth"
-intervalName 8 =
-    Ok "Minor sixth"
-intervalName 9 =
-    Ok "Major sixth"
-intervalName 10 =
-    Ok "Minor seventh"
-intervalName 11 =
-    Ok "Major seventh"
-intervalName 12 =
-    Ok "Perfect octave"
 intervalName n =
-    Error "not an interval name"
+    case Dict.get n intervalNames of
+        Just name ->
+            Result.Ok name
+
+        Nothing ->
+            Result.Err "not a named interval"
+
+
+
+-- to represent notes we will use MIDI integer representation
+-- https://en.wikipedia.org/wiki/Scientific_pitch_notation#Table_of_note_frequencies
+-- 12 semi tones in an octave:
+
+
+midiToOctave : Int -> Int
+midiToOctave n =
+    (n // 12) - 1
+
+
+
+-- returns an index into the noteNames list corresponding to the pitch of
+-- this MIDI number.
+
+
+midiToPitch : Int -> Int
+midiToPitch n =
+    modBy 12 n
