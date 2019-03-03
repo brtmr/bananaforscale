27ed342a7c4f5ab721bbba97c09624a741f47f8e
Bastian Reitemeier <mail@brtmr.de>
2019-03-03 13:54:43 +0100

    Added settings to the Model

--- src/Model.elm
+++ src/Model.elm
@@ -1,10 +1,22 @@
-module Model exposing (Flags, Model, Msg(..))
+module Model exposing (Flags, Model, Msg(..), startModel)
 
 import Browser.Dom exposing (Viewport)
+import Notes
 
 
 type alias Model =
-    { viewport : Maybe Viewport }
+    { viewport : Maybe Viewport
+    , scale : Notes.Scale
+    , drawHeadstock : Bool
+    }
+
+
+startModel : Model
+startModel =
+    { viewport = Nothing
+    , scale = Notes.majorScale
+    , drawHeadstock = True
+    }
 
 
 type Msg
